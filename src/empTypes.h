342d3b3fcc324d85740e2fec4f281ced4f9d3ebf
lucasdc <lucasdc@LucasN>
2016-09-08 16:55:13 -0300

    Add compress and silent item at the struct. Comments translated

--- src/empTypes.h
+++ src/empTypes.h
@@ -44,26 +44,32 @@ typedef struct{
 	char *path;
 }t_head_file;
 
-/*Estructura utilizada para el manejo de los archivos dentro del parseo*/
+/*Struct used in the parsing argument
+
+Estructura utilizada para el manejo de los archivos dentro del parseo*/
 
 typedef struct{
-	char **list;   //Lista de archivos
-	char *in;      //Nombre de archivo de entrada
-	char *out;     //Nombre de archivo de salida
-	size_t cant;   //Cantidad de archivos
-	size_t list_size;  //Tamaño total del arreglo
-	size_t size;   //Tamaño seteado para los Strings In y Out
-	size_t num_in; //Puede tomar valores 0 y 1
-	size_t num_out;//0=predeterminado, 1=nombre ingresado
+	char **list;   //Lista de archivos / List of files
+	char *in;      //Nombre de archivo de entrada / Name of input
+	char *out;     //Nombre de archivo de salida / Name of output
+	size_t compress;//Indica si se debe tratar con formato gz / It indicates whether to deal with format gz 
+	size_t silent; //Indica si se debe ejecutar el programa en modo silent / It indicates if the program is execute in silent mode 
+	size_t cant;   //Cantidad de archivos / Quantity of files
+	size_t list_size;  //Tamaño total del arreglo / Full size of the array
+	size_t size;   //Tamaño seteado para los Strings In y Out / Size of the strings In and Out
+	size_t num_in; //Puede tomar valores 0 y 1 				/ Can be 0 or 1
+	size_t num_out;//0=predeterminado, 1=nombre ingresado	/0 = default, 1 = input path 
 }files_array;
 
-/*Estructura principal utilizada para el parseo de argumentos*/
+/*Principal structure used in the parsing argument
+
+Estructura principal utilizada para el parseo de argumentos*/
 
 typedef struct{
-	char option[2]; //Arreglo de char con la opcion elegida
-	size_t op_index;//Index de la opcion
+	char option[2]; //Arreglo de char con la opcion elegida / Array of char with the option chosen
+	size_t op_index;//Index de la opcion / Index de la opcion
 	files_array operands;
-	int num_op;  //Siempre que tenga un valor diferente de 1 es error
+	int num_op;  //Siempre que tenga un valor diferente de 1 es error / If the value is diferent to 1 is error
 }option_args;
 
 #endif
