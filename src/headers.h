fad01d96b2bec0b3a87b33fbc1a1bef6f6d55719
lucasdc <lucasdc@LucasN>
2016-09-08 16:55:29 -0300

    Comments translated

--- src/headers.h
+++ src/headers.h
@@ -21,7 +21,9 @@ along with EMP. If not, see <http://www.gnu.org/licenses/>.     */
 #include <stdio.h>
 #include "empTypes.h"
 
-/*Genera el encabezado principal del paquete con todos los datos seteados predeterminadamente
+/*Generate the principal header from the package with all the default values
+
+Genera el encabezado principal del paquete con todos los datos seteados predeterminadamente
 Magic = EMP
 Version = 1
 Entradas = 0
@@ -29,25 +31,45 @@ Reservado 1 y 2 = 0
 */
 int create_head(t_head *);
 
-/*Genera el encabezado principal de cada archivo, recibe el path del archivo
+/*Generate the principal header from each file in the package, receives the path
+of the file to determinate the size and the date of the last modification.
+
+Genera el encabezado principal de cada archivo, recibe el path del archivo
 para determinar el Tamaño y la Fecha de Ultima Modificacion*/
 int create_head_file(t_head_file *, const char*);
 
+/*Write just one item of the structure
+
+Escribe un solo campo de la estructura*/
 void write_item(FILE*, void*, uint32_t, size_t);
 
-/*Carga al archivo principal el header principal*/
+/*Write at the principal file the principal header
+
+Escribe al archivo principal el header principal*/
 int write_head (FILE*, t_head*);
 
-/*Carga al archivo el header para cada archivo del paquete*/
+/*Write at the principal file the header from each file
+in the package
+
+Escribe al archivo el header para cada archivo del paquete*/
 int write_head_file(FILE*, t_head_file*);
 
+/*Read one item from the file
+
+Lee un solo campo del archivo*/
 void read_item(FILE*, void*, uint32_t, size_t);
 
-/*Lee el registro principal del archivo, ya se debe posicionar el puntero del
+/*Read the principal structure of the file. The function need the cursor at
+the beginning
+
+Lee la estructura principal del archivo, ya se debe posicionar el puntero del
 archivo antes de llamar la funcion*/
 int read_head(FILE*, t_head*);
 
-/*Lee el registro de cada archivo empaquetado del archivo, ya se debe posicionar
+/*Read the principal structure from each file packed. The function needs the cursor 
+correctly setted
+
+Lee la estructura de cada archivo empaquetado del archivo, ya se debe posicionar
 el puntero del archivo antes de llamar la funcion*/
 int read_head_file(FILE*, t_head_file*);
 
